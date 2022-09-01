30decb050e577ecc87444ca297b2afd8641e20e3
Seifbarouni <seif.barouni11@gmail.com>
2022-09-01 21:18:27 +0100

    initial commit

---
+++ extension.js
@@ -0,0 +1,33 @@
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const MainLoop = imports.mainloop;
+const GLib = imports.gi.GLib;
+
+let panelButton, panelButtonText, timeout;
+const SCRIPT_PATH = '/home/seif/Documents/other/scripts/ram-usage.sh'
+function setButtonText() {
+    const [ok, out, err, exit] = GLib.spawn_command_line_sync(SCRIPT_PATH)
+    panelButtonText.set_text("RAM usage: " + out.toString())
+    return true
+}
+
+function init() {
+    panelButton = new St.Bin({
+        style_class: "panel-button"
+    })
+    panelButtonText = new St.Label({
+        style_class: "panel",
+        text: "ram usage"
+    })
+    panelButton.set_child(panelButtonText)
+}
+
+function enable() {
+    Main.panel._leftBox.insert_child_at_index(panelButton, 1)
+    timeout = MainLoop.timeout_add_seconds(3.0, setButtonText)
+}
+
+function disable() {
+    MainLoop.source_remove(timeout)
+    Main.panel._leftBox.remove_child(panelButton, 1)
+}
